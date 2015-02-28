5bb213a93b128fc57981d530d492e4538de00f37
Felix Schulze <mail@felixschulze.com>
2015-02-28 21:34:59 +0000

    push changes

---
+++ rasp_src/README.md
@@ -0,0 +1,3 @@
+## Readme
+
+delete blacklist spi-bcm2708 from /etc/modprobe.d/raspi-blacklist.conf
--- rasp_src/main.c
+++ rasp_src/main.c
@@ -11,14 +11,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "mcp2515.h"
-
+#include "bcm2835.h"
 
 int main(int argc, char ** argv)
 {
 	uint8_t i = 0;
 
 	printf("Raspberry Pi CAN-Demo Code\n\rFelix Schulze 2015\n\rmail@felixschulze.com\n\r\n\r");
+
+	// check if program runs as root
+	if(getuid() != 0)
+	{
+		printf("Run program as root!\n\r");
+		return 1;
+	}
+
+	bcm2835_spi_begin();
+
+	bcm2835_spi_end();
 
 	return 0;
 }
