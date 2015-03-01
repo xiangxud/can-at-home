0aa736373260b9fa7794eb2d4e6b7ad107c6c732
Felix Schulze <mail@felixschulze.com>
2015-03-01 15:14:22 +0000

    get spi working

--- rasp_src/main.c
+++ rasp_src/main.c
@@ -28,8 +28,17 @@ int main(int argc, char ** argv)
 		return 1;
 	}
 
-	bcm2835_spi_begin();
+	if (!bcm2835_init())
+		return 1;
 
+	bcm2835_spi_begin();
+	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32);
+	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
+	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW); 
+	bcm2835_spi_transfer(0xaa);
+	bcm2835_spi_transfer(1);
 	bcm2835_spi_end();
 
 	return 0;
