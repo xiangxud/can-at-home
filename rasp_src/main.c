f0b757dc25100faf9d3cb4b61fce92b9b65be2f2
Felix Schulze <mail@felixschulze.com>
2015-03-07 14:49:33 +0100

    add beer ware license

--- rasp_src/main.c
+++ rasp_src/main.c
@@ -5,6 +5,12 @@
  * Desc:   RaspberryPi Can-Demo Code
  * Ver.:      0.02
  *
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <mail@felixschulze.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Felix Schulze
+ * ----------------------------------------------------------------------------
  */
 
 
